905ed2fa63098440147cdda161af060981ea5a52
victorBR116 <65189745+victorBR116@users.noreply.github.com>
2023-02-13 17:16:05 -0300

    adicionando apenas o links e classe da Globo

--- exibirNoticias.php
+++ exibirNoticias.php
@@ -4,3 +4,5 @@ require_once 'Noticia.php';
 
 $manchete = new Manchete("https://www.r7.com/", "r7-flex-title-h5__link"); // inserir o link do site de noticia e a class HTML.
 echo $manchete->pegarManchetes();
+
+//link e class do g1: "https://g1.globo.com/", "feed-post-link gui-color-primary gui-color-hover"
